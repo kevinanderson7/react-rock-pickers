ad7641bd657fe7b855390dc83072478a527c4cca
Kevin Anderson <kmichael419@hotmail.com>
2020-07-14 14:44:21 -0500

    increase and decrease buttons up and running

--- src/components/RockCounter/RockCounter.js
+++ src/components/RockCounter/RockCounter.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 
 class RockCounter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rocksPicked: 0,
+    };
+  }
+  clickIncrease = () => {
+    console.log('clicked increase');
+    this.setState({
+      rocksPicked: this.state.rocksPicked + 1,
+    });
+    console.log(this.state.rocksPicked);
+  };
+
+  clickDecrease = () => {
+    console.log('clicked decrease');
+    this.setState({
+      rocksPicked: this.state.rocksPicked - 1,
+    });
+    console.log(this.state.rocksPicked);
+  };
+
   render() {
     return (
       <div>
-          <div>
-              Rocks Picked: 0
-          </div>
-          <div>
-            <button>Increase</button>
-            <button>Decrease</button>
-            <button>Reset</button>
-          </div>
+        <div>Rocks Picked: {this.state.rocksPicked}</div>
+        <div>
+          <button onClick={this.clickIncrease}>Increase</button>
+          <button onClick={this.clickDecrease}>Decrease</button>
+          <button>Reset</button>
+        </div>
       </div>
     );
   }
